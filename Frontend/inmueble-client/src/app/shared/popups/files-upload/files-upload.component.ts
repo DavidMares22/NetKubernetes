3823d36fa9ddd2a3af364dfb22345fbdebe5b313
David Mares <damr18@gmail.com>
2025-03-22 07:50:43 -0600

    Enhance file upload functionality to support multiple file selection and add error handling

--- Frontend/inmueble-client/src/app/shared/popups/files-upload/files-upload.component.ts
+++ Frontend/inmueble-client/src/app/shared/popups/files-upload/files-upload.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  multiple: boolean;
+  crop: boolean;
+}
 
 @Component({
   selector: 'app-files-upload',
@@ -8,8 +14,15 @@ import { Component, OnInit } from '@angular/core';
 export class FilesUploadComponent implements OnInit {
 
   isHovering !: boolean;
+  files: File[] = [];
+  imageFile!: File;
+  isError!: boolean;
+  filesURLs: string[] = [];
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<FilesUploadComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +31,33 @@ export class FilesUploadComponent implements OnInit {
  
     this.isHovering = event;
   }
+
+  onDrop(files: FileList) {
+     this.dropGeneral(files);
+  }
+
+  onDropFile(event: FileList | any) {  
+    this.dropGeneral(event.target.files);
+  }
+
+  dropGeneral(files: FileList) {
+    this.isError = false;
+
+    if(this.data.crop && files.length > 1) {
+      this.isError = true;
+      return;
+    }
+
+    for (let i = 0; i < files.length; i++) {
+      this.files.push(files.item(i) as File);
+      }
+
+      console.log(files);
+      
+
+  }
+
+
 }
 
 
